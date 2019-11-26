958bddb19eb15683812fe69b2d7097fa2c656515
Tommi Mikkola <tommi.mikkola@tuni.fi>
2019-11-26 17:31:47 +0200

    add sawmill files

--- Game/sawmill.h
+++ Game/sawmill.h
@@ -1,11 +1,36 @@
-#ifndef SAWMILL_H
-#define SAWMILL_H
+#ifndef Sawmill_HH
+#define Sawmill_HH
 
 
-class Sawmill
+#include "buildings/buildingbase.h"
+#include "core/resourcemaps.h"
+#include "resourcemaps.hh"
+
+namespace Student {
+
+class Sawmill : public Course::BuildingBase
 {
 public:
-    Sawmill();
+
+    Sawmill() = delete;
+
+    explicit Sawmill(
+            const std::shared_ptr<Course::iGameEventHandler>& eventhandler,
+            const std::shared_ptr<Course::iObjectManager>& objectmanager,
+            const std::shared_ptr<Course::PlayerBase>& owner,
+            const int& tilespaces = 1,
+            const Course::ResourceMap& buildcost = Student::ConstResourceMaps::SAWMILL_BUILD_COST,
+            const Course::ResourceMap& production = Student::ConstResourceMaps::SAWMILL_PRODUCTION
+            );
+
+    virtual ~Sawmill() = default;
+
+    virtual std::string getType() const override;
+
+    virtual void onBuildAction() override;
+
 };
 
-#endif // SAWMILL_H
+}
+
+#endif // Sawmill_HH
