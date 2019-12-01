075b72b8861a63be32cdd6109ea6323584b56b2e
Tommi Brusila <tommi.brusila@tuni.fi>
2019-12-01 22:28:25 +0200

    Finish all the neccessary unittests

--- UnitTests/gameeventhandler/tst_gameeventhandlertest.cpp
+++ UnitTests/gameeventhandler/tst_gameeventhandlertest.cpp
@@ -29,36 +29,48 @@ void GameeventhandlerTest::testCase1()
 
 void GameeventhandlerTest::modifyResourcesTest()
 {
+    // Creata a resourcemap of costs where every cost is lower than player's resources
     const Course::ResourceMap enoughResources = {
-            {Course::MONEY, 2},
-            {Course::FOOD, 5},
-            {Course::WOOD, 1},
-            {Course::STONE, 1},
-            {Course::ORE, 0},
-        };
-        const Course::ResourceMap toolittleResources = {
-            {Course::MONEY, 2000},
-            {Course::FOOD, 5},
-            {Course::WOOD, 50000},
-            {Course::STONE, 1},
-            {Course::ORE, 0},
-        };
+        {Course::MONEY, 2},
+        {Course::FOOD, 5},
+        {Course::WOOD, 1},
+        {Course::STONE, 1},
+        {Course::ORE, 0},
+    };
 
-        int enoughMoney = 500;
-        int toolittleMoney = 2000;
+    // Creata a resourcemap of costs where some of the costs are higher than player's resources
+    const Course::ResourceMap toolittleResources = {
+        {Course::MONEY, 2000},
+        {Course::FOOD, 5},
+        {Course::WOOD, 50000},
+        {Course::STONE, 1},
+        {Course::ORE, 0},
+    };
 
-        Student::GameEventHandler evehandler;
-        std::shared_ptr<Student::Player> testPlayerStudent = std::make_shared<Student::Player>("testname");
+    // Create two int variables that are both over and under of the player's amount of money
+    int enoughMoney = 500;
+    int toolittleMoney = 2000;
 
-        bool test = evehandler.modifyResources(testPlayerStudent, enoughResources);
-        bool test2 = evehandler.modifyResources(testPlayerStudent, toolittleResources);
-        bool test3 = evehandler.modifyResource(testPlayerStudent, Course::MONEY, enoughMoney);
-        bool test4= evehandler.modifyResource(testPlayerStudent, Course::MONEY, toolittleMoney);
+    // Create the eventHandler and the student type player
+    Student::GameEventHandler evehandler;
+    std::shared_ptr<Student::Player> testPlayerStudent = std::make_shared<Student::Player>("testname");
 
-        QVERIFY(test == true);
-        QVERIFY(test2 == false);
-        QVERIFY(test3 == true);
-        QVERIFY(test4 == false);
+    // Test modifyResources method when the player has enough resources
+    bool test = evehandler.modifyResources(testPlayerStudent, enoughResources);
+
+    // Test modifyResources method when the player doesn't have enough resources
+    bool test2 = evehandler.modifyResources(testPlayerStudent, toolittleResources);
+
+    // Test modifyResource method when the player has enough of the resource
+    bool test3 = evehandler.modifyResource(testPlayerStudent, Course::MONEY, enoughMoney);
+
+    // Test modifyResource method when the player doesn't have enough of the resource
+    bool test4= evehandler.modifyResource(testPlayerStudent, Course::MONEY, toolittleMoney);
+
+    QVERIFY(test == true);
+    QVERIFY(test2 == false);
+    QVERIFY(test3 == true);
+    QVERIFY(test4 == false);
 }
 
 QTEST_APPLESS_MAIN(GameeventhandlerTest)
--- UnitTests/objectmanager/tst_objectmanagertest.cpp
+++ UnitTests/objectmanager/tst_objectmanagertest.cpp
@@ -33,47 +33,55 @@ void ObjectmanagerTest::testCase1()
 
 void ObjectmanagerTest::tileTest()
 {
+    // Create the objectManager and the gameEventHandler
     std::shared_ptr<Student::ObjectManager> objManager = std::make_shared<Student::ObjectManager>();
     std::shared_ptr<Student::GameEventHandler> eveHandler = std::make_shared<Student::GameEventHandler>();
 
+    // Create the coordinates for the soon to be created tiles
     Course::Coordinate mountainTileCoordinate(0,0);
     Course::Coordinate cobbleTileCoordinate(10,10);
     Course::Coordinate forestTileCoordinate(20, 20);
     Course::Coordinate otherForestTileCoordinate(50, 30);
-
-
     Course::Coordinate forestNeighbourCoordinate(20, 21);
 
-    Course::Coordinate noneTileCoordinate(25, 5);
-
-
+    // Create the tiles from the previously created coordinates
     std::shared_ptr<Student::Mountain> mountainTile = std::make_shared<Student::Mountain>(mountainTileCoordinate, eveHandler, objManager);
     std::shared_ptr<Student::Cobblestone> cobbleTile = std::make_shared<Student::Cobblestone>(cobbleTileCoordinate, eveHandler, objManager);
     std::shared_ptr<Course::Forest> forestTile = std::make_shared<Course::Forest>(forestTileCoordinate, eveHandler, objManager);
     std::shared_ptr<Student::Mountain> forestNeighbour = std::make_shared<Student::Mountain>(forestNeighbourCoordinate, eveHandler, objManager);
     std::shared_ptr<Course::Forest> otherForestTile = std::make_shared<Course::Forest>(otherForestTileCoordinate, eveHandler, objManager);
 
+    // Create a vector of some of the tiles and add it to the
     std::vector<std::shared_ptr<Course::TileBase>> tileVector = {mountainTile, cobbleTile, forestTile, forestNeighbour};
-
     objManager->addTiles(tileVector);
 
+    // Test if the addTiles method and returnTiles method "have" the same tiles as the manually created tileVector
     bool test1 = (objManager->returnTiles() == tileVector);
+
+    // Test if the getTile(coordinate) gives the right tile
     bool test2 = (objManager->getTile(forestTileCoordinate)->getType() == tileVector.at(2)->getType());
+
+    // Test if the getTile(id) gives the right tile
     bool test3 = (objManager->getTile(1)->getType() == tileVector.at(1)->getType());
 
+    // Createa a vector of some of the coordinates and a vector of tiles that have those coordinates in the same order
     std::vector<Course::Coordinate> coordinateVector = {mountainTileCoordinate, cobbleTileCoordinate};
     std::vector<std::shared_ptr<Course::TileBase>> tileVectorForCoordinates = {mountainTile, cobbleTile};
 
+    // Test if the getTiles(vector with coordinates) returns a vector with the right tiles
     bool test4 = (objManager->getTiles(coordinateVector) == tileVectorForCoordinates);
 
+    // Get a vector of the forestTile's neighbours
     std::vector<std::shared_ptr<Course::GameObject>> neighbourTiles = objManager->getNeighbourTiles(forestTile);
 
+    // Test if the last tile (first tile is the tested tile itself) has the same type as the supposed neighbour tile
     bool test5 = (neighbourTiles.size() > 0 && neighbourTiles.at(neighbourTiles.size()-1)->getType() == forestNeighbour->getType());
 
+    // Create a new vector of the previously created tiles and update the objectManager's vector of tiles
     std::vector<std::shared_ptr<Course::TileBase>> otherTileVector = {forestTile, cobbleTile, otherForestTile};
-
     objManager->updateTileVector(otherTileVector);
 
+    // Test if the returnTiles() returns a vector with the right tiles
     bool test6 = (objManager->returnTiles() == otherTileVector);
 
     QVERIFY(test1 == true);
