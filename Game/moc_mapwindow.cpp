e5eee791eadc57604e57c89ee1ab66d062a00fa6
Tommi Mikkola <tommi.mikkola@tuni.fi>
2019-12-02 02:29:23 +0200

    fixing comments

--- Game/moc_mapwindow.cpp
+++ Game/moc_mapwindow.cpp
@@ -1,18 +1,19 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mapwindow.hh'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.6)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.13.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
+#include <memory>
 #include "mapwindow.hh"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mapwindow.hh' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.9.6. It"
+#error "This file was generated using the moc from 5.13.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -41,7 +42,7 @@ QT_MOC_LITERAL(0, 0, 9) // "MapWindow"
 static const uint qt_meta_data_MapWindow[] = {
 
  // content:
-       7,       // revision
+       8,       // revision
        0,       // classname
        0,    0, // classinfo
        0,    0, // methods
@@ -62,10 +63,14 @@ void MapWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
     Q_UNUSED(_a);
 }
 
-const QMetaObject MapWindow::staticMetaObject = {
-    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MapWindow.data,
-      qt_meta_data_MapWindow,  qt_static_metacall, nullptr, nullptr}
-};
+QT_INIT_METAOBJECT const QMetaObject MapWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_MapWindow.data,
+    qt_meta_data_MapWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
 
 
 const QMetaObject *MapWindow::metaObject() const
